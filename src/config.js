18d701401cacc4653e557d216460b22342ce0972
Ngo <Kean.Ngo@team.telstra.com>
2019-12-03 21:59:13 +1100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-southeast-2",
+      BUCKET: "notes-app-upload-kean"
+    },
+    apiGateway: {
+      REGION: "ap-southeast-2",
+      URL: "https://8vya9ehfwb.execute-api.ap-southeast-2.amazonaws.com/prod/"
+    },
+    cognito: {
+      REGION: "ap-southeast-2",
+      USER_POOL_ID: "ap-southeast-2_Tu74ksy94",
+      APP_CLIENT_ID: "2at9o9qbb35c41g21k0tcdl3j",
+      IDENTITY_POOL_ID: "ap-southeast-2:6656273e-af21-4f93-9f27-25c296ed06c6"
+    }
+  };
